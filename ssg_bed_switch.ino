d6638c2c7adb5cfb7ba6fbda1daf06b0a3e78197
Hepheir <hepheir@gmail.com>
2023-06-01 21:21:46 +0900

    test: successful relay but failed serial

--- ssg_bed_switch.ino
+++ ssg_bed_switch.ino
@@ -1,15 +1,39 @@
+#include <string.h>
 #include "relay.h"
+#include "serial.h"
 
-Relay relay = Relay();
+Relay *relay;
+SerialHandler *serial_handler;
 
 void setup() {
-  relay.setSwitchPin(3);
-  relay.open();
+  SoftwareSerialBuilder *builder;
+  SoftwareSerial *serial;
+
+  relay = new Relay();
+  relay->setSwitchPin(2);
+
+  builder = new SoftwareSerialBuilder();
+  builder->setRxPin(3);
+  builder->setTxPin(4);
+  builder->setBaudRate(9600);
+
+  serial = builder->buildAndListen();
+  serial_handler = new SerialHandler(serial);
+
+  free(builder);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  delay(1000);
-  relay.open();
-  delay(1000);
-  relay.close();
+  const char *data = serial_handler->receive();
+  if (strcmp(data, "INSLEEP=FALSE") == 0) {
+    relay->close();
+    Serial.println("[received] insleep: false");
+  }
+  else if (strcmp(data, "INSLEEP=TRUE") == 1) {
+    relay->open();
+    Serial.println("[received] insleep: true");
+  }
+  delay(100);
 }
