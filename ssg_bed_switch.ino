bc58a081ec579b004f858c87afdb68972fa6330b
Hepheir <hepheir@gmail.com>
2023-06-01 16:45:53 +0900

    feat: create test code

--- ssg_bed_switch.ino
+++ ssg_bed_switch.ino
@@ -1,7 +1,15 @@
-void setup() {
+#include "relay.h"
 
+Relay relay = Relay();
+
+void setup() {
+  relay.setSwitchPin(3);
+  relay.open();
 }
 
 void loop() {
-
+  delay(1000);
+  relay.open();
+  delay(1000);
+  relay.close();
 }
